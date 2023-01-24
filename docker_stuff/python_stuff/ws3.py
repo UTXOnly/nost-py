e6699ccc70c9cab6ac35af4cab3217287eb09274
Brian Hartford <bhartford419@gmail.com>
2023-01-23 21:14:36 -0500

    Addtag filter class

--- docker_stuff/python_stuff/ws3.py
+++ docker_stuff/python_stuff/ws3.py
@@ -6,9 +6,9 @@ import hmac
 import hashlib
 from time import time
 #from ddtrace import tracer
-from sqlalchemy import create_engine, Column, String, Integer, JSON, Query
+from sqlalchemy import create_engine, Column, String, Integer, JSON, #Query
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Query
 from sqlalchemy.ext.declarative import declarative_base
 import logging
 
