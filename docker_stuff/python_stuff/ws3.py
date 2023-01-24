eb13f8689d03a96e7779786ae2383248257df238
Brian Hartford <bhartford419@gmail.com>
2023-01-23 23:09:33 -0500

    Add response debug

--- docker_stuff/python_stuff/ws3.py
+++ docker_stuff/python_stuff/ws3.py
@@ -173,10 +173,10 @@ async def event_handler(websocket, path):
                             elif filter_name == "until":
                                 query = query.filter(Event.created_at <= filter_value)
                                 logging.debug(f"Filtering events created until: {filter_value}")
-                            elif filter_name == "e_tags":
+                            elif filter_name == "#e":
                                 query = query.filter(Event.e_tags.any(lambda tag: tag["value"] in filter_value))
                                 logging.debug(f"Filtering events e tags: {filter_value}")
-                            elif filter_name == "p_tags":
+                            elif filter_name == "#p":
                                 query = query.filter(Event.p_tags.any(lambda tag: tag["value"] in filter_value))
                                 logging.debug(f"Filtering events p tags: {filter_value}")
                             elif filter_name == "limit":
