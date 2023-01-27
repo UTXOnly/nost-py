af9ffd09905b921820f32eb631b2960109440e02
Brian Hartford <bhartford419@gmail.com>
2023-01-26 20:47:31 -0500

    Add test to query

--- docker_stuff/python_stuff/ws3.py
+++ docker_stuff/python_stuff/ws3.py
@@ -202,7 +202,7 @@ async def event_handler(websocket, path):
                             #    logging.debug(f"Filtering limits: {filter_value}")
                 
                         try:
-                            results = db.query(Event).all()
+                            results = db.query().all()
                             logging.debug("Entries: %s", results)
     
                             #results = query.all()
