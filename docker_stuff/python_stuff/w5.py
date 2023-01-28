c7db07001683943c2246c54c086fcd13bc1155db
Brian Hartford <bhartford419@gmail.com>
2023-01-27 23:43:41 -0500

    Cleaning up, trying new

---
+++ docker_stuff/python_stuff/w5.py
@@ -0,0 +1,103 @@
+import os
+import json
+import hmac
+import hashlib
+import logging
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, String, Integer, JSON, ARRAY, text, cast, Text
+
+def calc_event_id(public_key:str, created_at:int, kind_number:int, tags:list, content:str) -> str:
+    """
+    This function calculates the event id using the provided information
+    """
+    # do some computation to calculate event id
+    event_id = hashlib.sha256((str(public_key) + str(created_at) + str(kind_number) + str(tags) + str(content)).encode()).hexdigest()
+    return event_id
+
+class Event:
+    __tablename__ = "event_table"
+    id = Column(String, primary_key=True)
+    pubkey = Column(String)
+    kind = Column(Integer)
+    created_at = Column(Integer)
+    tags = Column(Text)
+    content = Column(String)
+    sig = Column(String)
+    def __init__(self, pubkey:str, created_at:int, kind:int, tags:list, content:str):
+        self.pubkey = pubkey
+        self.created_at = created_at
+        self.kind = kind
+        self.tags = tags
+        self.content = content
+    def calc_id(self):
+        return calc_event_id(
+            public_key=self.pubkey,
+            created_at=self.created_at,
+            kind_number=self.kind,
+            tags=[tag.serialize() for tag in self.tags],
+            content=self.content,
+        )
+    def get_tags_keys(self, tag_type: str):
+        return set(tag.key for tag in self.tags if tag.type == tag_type)
+    @property
+    def e_tags(self):
+        return self.get_tags_keys("e")
+    @property
+    def p_tags(self):
+        return self.get_tags_keys("p")
+
+def save_event(received_data: dict):
+    event = Event.from_dict(received_data)
+    session = Session()
+    session.add(event)
+    session.commit()
+    session.close()
+
+def notify_connected_clients(received_data: dict):
+    event = Event.from_dict(received_data)
+    for ws in connected_websockets:
+        ws.send(event.to_json())
+connected_websockets = set()
+async def event_handler(websocket, path):
+    connected_websockets.add(websocket)
+    while True:
+        try:
+            event_data = await websocket.recv()
+            logging.debug(f"Received event: {event_data}")
+            received_data = json.loads(event_data)
+                       # Verify the signature of the event
+            secret_key = "your_secret_key"
+            signature = received_data["signature"]
+            del received_data["signature"]
+            message = json.dumps(received_data)
+            computed_signature = hmac.new(secret_key.encode(), message.encode(), hashlib.sha256).hexdigest()
+            if not hmac.compare_digest(computed_signature, signature):
+                raise ValueError("Invalid signature")
+            
+            # Save the event to the database
+            save_event(received_data)
+            
+            # Notify connected websockets of the new event
+            notify_connected_clients(received_data)
+
+            if event_data[0] == "EVENT":
+                event = event_data[1]
+                id = event.get("id")
+                pubkey = event.get("pubkey")
+                created_at = event.get("created_at")
+                kind = event.get("kind")
+                tags = event.get("tags")
+                content = event.get("content")
+                sig = event.get("sig")
+        except Exception as e:
+            logging.exception(e)
+            break
+        finally:
+            connected_websockets.remove(websocket)
+    
+DATABASE_URL = os.environ.get("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
