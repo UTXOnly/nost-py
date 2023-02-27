768041e3a2e33df9d2bb167187a7b6fdaa41dea7
Brian Hartford <bhartford419@gmail.com>
2023-02-26 21:04:34 -0500

    Iforgot

--- docker_stuff/nginx/init.sh
+++ docker_stuff/nginx/init.sh
@@ -3,7 +3,7 @@
 # Check if certbot command was already run
 if [[ -z "${CERTBOT_RUN}" ]]; then
   # Run certbot command
-  certbot run -n --nginx --agree-tos -d nostrtest.dojotunnel.online  -m bhartford419@gmail.com --redirect
+  certbot run -n --nginx --agree-tos -d no.dojotunnel.online  -m bhartford419@gmail.com --redirect
   export CERTBOT_RUN=true
 fi
 
