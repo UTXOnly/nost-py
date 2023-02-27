53b7d3a345b8d3ceb03f84bb070057468d23ea18
Brian Hartford <bhartford419@gmail.com>
2023-02-26 20:32:43 -0500

    Nginx add init

---
+++ docker_stuff/nginx/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if certbot command was already run
+if [[ -z "${CERTBOT_RUN}" ]]; then
+  # Run certbot command
+  certbot run -n --nginx --agree-tos -d nostrtest.dojotunnel.online  -m bhartford419@gmail.com --redirect
+  export CERTBOT_RUN=true
+fi
+
+# Reload nginx
+nginx -s reload
+
+# Keep container running
+nginx -g 'daemon off;'
